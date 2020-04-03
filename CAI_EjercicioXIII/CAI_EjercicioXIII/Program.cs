0b4203cb7e10ab3eb9f0c7e1fbafdb7a195e660d
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-04-02 21:21:50 -0300

    EjercicioXIII

---
+++ CAI_EjercicioXIII/CAI_EjercicioXIII/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioXIII
+{
+    class Program
+    {
+        static int factorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            return num * factorial(num - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+            int ingreso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su factorial es: " + factorial(ingreso));
+            Console.ReadKey(true);
+        }
+    }
+}
